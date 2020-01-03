fc9e2f4febe0eafcbfd3d9ff20c7b9b16e082941
Florent Flament <contact@florentflament.com>
2020-01-04 00:06:08 +0100

    Tune lot2 timings

--- src/slideshow-lot2-timeline.asm
+++ src/slideshow-lot2-timeline.asm
@@ -1,39 +1,90 @@
 slideshow_lot2_l:
 	dc.b <slideshow_07_ptr
 	dc.b <slideshow_08_ptr
+
+	;; Pils and love
 	dc.b <slideshow_09_a_ptr
 	dc.b <slideshow_09_b_ptr
+	dc.b <slideshow_09_a_ptr
+	dc.b <slideshow_09_b_ptr
+	dc.b <slideshow_09_a_ptr
+	dc.b <slideshow_09_b_ptr
+	dc.b <slideshow_09_a_ptr
+	dc.b <slideshow_09_b_ptr
+
+	;; Deviation
 	dc.b <slideshow_10_a_ptr
 	dc.b <slideshow_10_b_ptr
+	dc.b <slideshow_10_a_ptr
 	dc.b <slideshow_10_c_ptr
+
+	;; Fritkot
 	dc.b <slideshow_11_a_ptr
 	dc.b <slideshow_11_b_ptr
 	dc.b <slideshow_11_c_ptr
+	dc.b <slideshow_11_a_ptr
+	dc.b <slideshow_11_b_ptr
+	dc.b <slideshow_11_c_ptr
+
 	dc.b <slideshow_12_a_ptr
-	
+
 slideshow_lot2_h:
 	dc.b >slideshow_07_ptr
 	dc.b >slideshow_08_ptr
+
+	;; Pils and love
 	dc.b >slideshow_09_a_ptr
 	dc.b >slideshow_09_b_ptr
+	dc.b >slideshow_09_a_ptr
+	dc.b >slideshow_09_b_ptr
+	dc.b >slideshow_09_a_ptr
+	dc.b >slideshow_09_b_ptr
+	dc.b >slideshow_09_a_ptr
+	dc.b >slideshow_09_b_ptr
+
+	;; Deviation
 	dc.b >slideshow_10_a_ptr
 	dc.b >slideshow_10_b_ptr
+	dc.b >slideshow_10_a_ptr
 	dc.b >slideshow_10_c_ptr
+
+	;; Fritkot
 	dc.b >slideshow_11_a_ptr
 	dc.b >slideshow_11_b_ptr
 	dc.b >slideshow_11_c_ptr
+	dc.b >slideshow_11_a_ptr
+	dc.b >slideshow_11_b_ptr
+	dc.b >slideshow_11_c_ptr
+
 	dc.b >slideshow_12_a_ptr
-	
+
 slideshow_lot2_t:
 	dc.b 112
 	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
+
+	;; Pils and love
+	dc.b 14
+	dc.b 14
+	dc.b 14
+	dc.b 14
+	dc.b 14
+	dc.b 14
+	dc.b 14
+	dc.b 14
+
+	;; Deviation
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+
+	;; Fritkot
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+
 	dc.b 112
 	dc.b 0
