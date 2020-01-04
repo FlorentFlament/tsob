5dcd68d807e9fb5d90399be6181cde7d6ed4f946
Florent Flament <contact@florentflament.com>
2020-01-04 23:03:32 +0100

    Update Fritkot timing

--- src/slideshow-lot2-timeline.asm
+++ src/slideshow-lot2-timeline.asm
@@ -25,6 +25,12 @@ slideshow_lot2_l:
 	dc.b <slideshow_11_a_ptr
 	dc.b <slideshow_11_b_ptr
 	dc.b <slideshow_11_c_ptr
+	dc.b <slideshow_11_a_ptr
+	dc.b <slideshow_11_b_ptr
+	dc.b <slideshow_11_c_ptr
+	dc.b <slideshow_11_a_ptr
+	dc.b <slideshow_11_b_ptr
+	dc.b <slideshow_11_c_ptr
 
 	dc.b <slideshow_12_a_ptr
 
@@ -55,6 +61,12 @@ slideshow_lot2_h:
 	dc.b >slideshow_11_a_ptr
 	dc.b >slideshow_11_b_ptr
 	dc.b >slideshow_11_c_ptr
+	dc.b >slideshow_11_a_ptr
+	dc.b >slideshow_11_b_ptr
+	dc.b >slideshow_11_c_ptr
+	dc.b >slideshow_11_a_ptr
+	dc.b >slideshow_11_b_ptr
+	dc.b >slideshow_11_c_ptr
 
 	dc.b >slideshow_12_a_ptr
 
@@ -79,12 +91,18 @@ slideshow_lot2_t:
 	dc.b 56
 
 	;; Fritkot
-	dc.b 56
-	dc.b 56
-	dc.b 56
-	dc.b 56
-	dc.b 56
-	dc.b 56
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
 
 	dc.b 112
 	dc.b 0
