e71b3d6ba61f0933b20c3c387e621a08e8f91b12
Florent Flament <contact@florentflament.com>
2020-01-04 23:21:12 +0100

    Update lot 5 timings

--- src/slideshow-lot5-timeline.asm
+++ src/slideshow-lot5-timeline.asm
@@ -1,39 +1,77 @@
 slideshow_lot5_l:
 	dc.b <slideshow_23_b_ptr
+
 	dc.b <slideshow_22_a_ptr
 	dc.b <slideshow_22_b_ptr
 	dc.b <slideshow_22_c_ptr
 	dc.b <slideshow_22_d_ptr
 	dc.b <slideshow_22_e_ptr
 	dc.b <slideshow_22_f_ptr
+	dc.b <slideshow_22_a_ptr
+	dc.b <slideshow_22_b_ptr
+	dc.b <slideshow_22_c_ptr
+	dc.b <slideshow_22_d_ptr
+	dc.b <slideshow_22_e_ptr
+	dc.b <slideshow_22_f_ptr
+
 	dc.b <slideshow_24_a_ptr
 	dc.b <slideshow_24_b_ptr
 	dc.b <slideshow_24_c_ptr
 	dc.b <slideshow_24_d_ptr
-	
+	dc.b <slideshow_24_a_ptr
+	dc.b <slideshow_24_b_ptr
+	dc.b <slideshow_24_c_ptr
+	dc.b <slideshow_24_d_ptr
+
 slideshow_lot5_h:
 	dc.b >slideshow_23_b_ptr
+
 	dc.b >slideshow_22_a_ptr
 	dc.b >slideshow_22_b_ptr
 	dc.b >slideshow_22_c_ptr
 	dc.b >slideshow_22_d_ptr
 	dc.b >slideshow_22_e_ptr
 	dc.b >slideshow_22_f_ptr
+	dc.b >slideshow_22_a_ptr
+	dc.b >slideshow_22_b_ptr
+	dc.b >slideshow_22_c_ptr
+	dc.b >slideshow_22_d_ptr
+	dc.b >slideshow_22_e_ptr
+	dc.b >slideshow_22_f_ptr
+
 	dc.b >slideshow_24_a_ptr
 	dc.b >slideshow_24_b_ptr
 	dc.b >slideshow_24_c_ptr
 	dc.b >slideshow_24_d_ptr
-	
+	dc.b >slideshow_24_a_ptr
+	dc.b >slideshow_24_b_ptr
+	dc.b >slideshow_24_c_ptr
+	dc.b >slideshow_24_d_ptr
+
 slideshow_lot5_t:
 	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
+
+	;; 22 A/B/C/D/E/F
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+
+	;; 24 A/B/C/D
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
 	dc.b 0			; end
