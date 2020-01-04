63f3dba0c102ac6710855079bfcd15b7858e9ed0
Florent Flament <contact@florentflament.com>
2020-01-04 23:28:24 +0100

    Update lot 7 timigs

--- src/slideshow-lot7-timeline.asm
+++ src/slideshow-lot7-timeline.asm
@@ -1,39 +1,64 @@
 slideshow_lot7_l:
 	dc.b <slideshow_25_a_ptr
 	dc.b <slideshow_25_b_ptr
+	dc.b <slideshow_25_a_ptr
+	dc.b <slideshow_25_b_ptr
+
 	dc.b <slideshow_29_a_ptr
 	dc.b <slideshow_29_b_ptr
+
 	dc.b <slideshow_30_a_ptr
 	dc.b <slideshow_30_b_ptr
+	dc.b <slideshow_30_a_ptr
+	dc.b <slideshow_30_b_ptr
+
 	dc.b <slideshow_31_a_ptr
 	dc.b <slideshow_31_b_ptr
 	dc.b <slideshow_31_c_ptr
 	dc.b <slideshow_31_d_ptr
+
 	dc.b <slideshow_32_ptr
-	
+
 slideshow_lot7_h:
 	dc.b >slideshow_25_a_ptr
 	dc.b >slideshow_25_b_ptr
+	dc.b >slideshow_25_a_ptr
+	dc.b >slideshow_25_b_ptr
+
 	dc.b >slideshow_29_a_ptr
 	dc.b >slideshow_29_b_ptr
+
 	dc.b >slideshow_30_a_ptr
 	dc.b >slideshow_30_b_ptr
+	dc.b >slideshow_30_a_ptr
+	dc.b >slideshow_30_b_ptr
+
 	dc.b >slideshow_31_a_ptr
 	dc.b >slideshow_31_b_ptr
 	dc.b >slideshow_31_c_ptr
 	dc.b >slideshow_31_d_ptr
+
 	dc.b >slideshow_32_ptr
-	
+
 slideshow_lot7_t:
+	;; 25
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	;; 29
+	dc.b 112
+	dc.b 112
+	;; 30
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	;; 31
 	dc.b 112
 	dc.b 112
 	dc.b 112
 	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
+	;; 32
 	dc.b 112
 	dc.b 0			; end
