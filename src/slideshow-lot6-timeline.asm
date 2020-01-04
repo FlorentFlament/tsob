8fefbbff612427b61c55589a0954ee97ccf95490
Florent Flament <contact@florentflament.com>
2020-01-04 23:24:58 +0100

    Update lot 6 timings

--- src/slideshow-lot6-timeline.asm
+++ src/slideshow-lot6-timeline.asm
@@ -1,8 +1,18 @@
 slideshow_lot6_l:
 	dc.b <slideshow_26_a_ptr
 	dc.b <slideshow_26_b_ptr
+	dc.b <slideshow_26_a_ptr
+	dc.b <slideshow_26_b_ptr
+
 	dc.b <slideshow_27_a_ptr
 	dc.b <slideshow_27_b_ptr
+	dc.b <slideshow_27_a_ptr
+	dc.b <slideshow_27_b_ptr
+	dc.b <slideshow_27_a_ptr
+	dc.b <slideshow_27_b_ptr
+	dc.b <slideshow_27_a_ptr
+	dc.b <slideshow_27_b_ptr
+
 	dc.b <slideshow_28_a_ptr
 	dc.b <slideshow_28_b_ptr
 	dc.b <slideshow_28_c_ptr
@@ -10,12 +20,29 @@ slideshow_lot6_l:
 	dc.b <slideshow_28_e_ptr
 	dc.b <slideshow_28_f_ptr
 	dc.b <slideshow_28_g_ptr
-	
+	dc.b <slideshow_28_a_ptr
+	dc.b <slideshow_28_b_ptr
+	dc.b <slideshow_28_c_ptr
+	dc.b <slideshow_28_d_ptr
+	dc.b <slideshow_28_e_ptr
+	dc.b <slideshow_28_f_ptr
+	dc.b <slideshow_28_g_ptr
+
 slideshow_lot6_h:
 	dc.b >slideshow_26_a_ptr
 	dc.b >slideshow_26_b_ptr
+	dc.b >slideshow_26_a_ptr
+	dc.b >slideshow_26_b_ptr
+
 	dc.b >slideshow_27_a_ptr
 	dc.b >slideshow_27_b_ptr
+	dc.b >slideshow_27_a_ptr
+	dc.b >slideshow_27_b_ptr
+	dc.b >slideshow_27_a_ptr
+	dc.b >slideshow_27_b_ptr
+	dc.b >slideshow_27_a_ptr
+	dc.b >slideshow_27_b_ptr
+
 	dc.b >slideshow_28_a_ptr
 	dc.b >slideshow_28_b_ptr
 	dc.b >slideshow_28_c_ptr
@@ -23,17 +50,42 @@ slideshow_lot6_h:
 	dc.b >slideshow_28_e_ptr
 	dc.b >slideshow_28_f_ptr
 	dc.b >slideshow_28_g_ptr
-	
+	dc.b >slideshow_28_a_ptr
+	dc.b >slideshow_28_b_ptr
+	dc.b >slideshow_28_c_ptr
+	dc.b >slideshow_28_d_ptr
+	dc.b >slideshow_28_e_ptr
+	dc.b >slideshow_28_f_ptr
+	dc.b >slideshow_28_g_ptr
+
 slideshow_lot6_t:
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
-	dc.b 112
+	;; 26
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	;; 27
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	dc.b 28
+	;; 28
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
+	dc.b 56
 	dc.b 0			; end
